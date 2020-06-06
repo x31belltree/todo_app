b0337ef1ad8a8d50fab561214fbcb05d6128239c
x31belltree <belltree.masaki@gmail.com>
2020-06-06 14:33:55 +0000

    update

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,13 +1,10 @@
 class GoalsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_goal, only: [:show, :edit, :update, :destroy]
 
   # GET /goals
   def index
-    @goals = Goal.all
-  end
-
-  # GET /goals/1
-  def show
+    @goals = current_user.goals
   end
 
   # GET /goals/new
@@ -23,8 +20,7 @@ class GoalsController < ApplicationController
   def create
     @goal = Goal.new(goal_params)
 
-    if @goal.save
-      redirect_to @goal, notice: 'Goal was successfully created.'
+    if @goal.save      redirect_to @goal, notice: 'Goal was successfully created.'
     else
       render :new
     end
@@ -33,7 +29,6 @@ class GoalsController < ApplicationController
   # PATCH/PUT /goals/1
   def update
     if @goal.update(goal_params)
-      redirect_to @goal, notice: 'Goal was successfully updated.'
     else
       render :edit
     end
@@ -42,16 +37,14 @@ class GoalsController < ApplicationController
   # DELETE /goals/1
   def destroy
     @goal.destroy
-    redirect_to goals_url, notice: 'Goal was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_goal
-      @goal = Goal.find(params[:id])
+      @goal = current_user.goals.find_by(params[:id])
+      redirect_to(goals_url, alert: "ERROR!!") if @goal.blank?
     end
 
-    # Only allow a trusted parameter "white list" through.
     def goal_params
       params.require(:goal).permit(:title, :user_id)
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :todos
-  resources :goals
+  resources :goals do
+    resources :todos
+      member do
+        get "sort"
+      end
+  end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
